6242fb3d7fcf01a6c256636c180ebf04d074e852
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2023-10-25 16:15:45 +1100

    Update patch to the latest Emby beta

--- patch.sh
+++ patch.sh
@@ -11,12 +11,11 @@ mkdir ./tmp && cd ./tmp
 
 #Patch web client
 ../ildasm $EMBY_PATH/system/Emby.Web.dll -out=Emby.Web.dll
-sed -i 's#ajax({url:"https://mb3admin.com/admin/service/registration/validateDevice?"+paramsToString(params),type:"POST",dataType:"json"})#Promise.resolve(new Response('"'"'{"cacheExpirationDays":365,"message":"Device Valid","resultCode":"GOOD"}'"'"').json())#g' Emby.Web.dashboard_ui.modules.emby_apiclient.connectionmanager.js
+sed -i 's#ajax({url:"https://mb3admin.com/admin/service/registration/validateDevice?"+new URLSearchParams(params).toString(),type:"POST",dataType:"json"})#Promise.resolve(new Response('"'"'{"cacheExpirationDays":365,"message":"Device Valid","resultCode":"GOOD"}'"'"').json())#g' Emby.Web.dashboard_ui.modules.emby_apiclient.connectionmanager.js
 ../ilasm -dll Emby.Web.dll -out=$EMBY_PATH/system/Emby.Web.dll
 rm Emby.Web.*
 
-# TODO
-# sed -i 's/mb3admin.com/tsumo.cf/g' $EMBY_PATH/system/dashboard-ui/embypremiere/embypremiere.js
+sed -i 's#fetch("https://mb3admin.com/admin/service/registration/getStatus",{method:"POST",body:key,headers:{"Content-Type":"application/x-www-form-urlencoded"}}).then(function(response){return response.json()})#Promise.resolve('"'"'{"planType":"Lifetime","deviceStatus":0,"subscriptions":[]}'"'"')#g' $EMBY_PATH/system/dashboard-ui/embypremiere/embypremiere.js
 
 # Delete tmp
 cd ../ && rm -rf tmp
